85edaaf8ecf513ef76ba02d49477cb31513fb5c2
MD SIFAT KHAN <sifatkhann229@gmail.com>
2022-12-07 07:01:52 +0800

    login page setup

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -35,11 +35,11 @@ const Navbar = () => {
                         {menuItems}
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">Doctors Portal</a>
+                <a className="btn btn-ghost normal-case text-xl">PentaBit Hospital</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
-                    {menuItems}
+                    {menuItems}  
                 </ul>
             </div>
             <div className="navbar-end">
